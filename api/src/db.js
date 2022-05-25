0b3a3d2fc671a19b47736525d2b6f3f49e581cb4
juanmabascu997 <jmb972012@gmail.com>
2022-05-25 16:55:50 -0300

    Commit

--- api/src/db.js
+++ api/src/db.js
@@ -3,12 +3,19 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DATABASE_URL
 } = process.env;
 //postgres://juanmabacu:juanma12@localhost/dogs
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/dogs`, {
+const sequelize = new Sequelize(DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  
+  dialectOptions:{
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },  
 });
 const basename = path.basename(__filename);
 
